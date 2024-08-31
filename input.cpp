0c20247ad25a8b5dd66567ac2637e4a53f78e1d7
Divit <divitkalathil@gmail.com>
2024-08-31 12:14:31 -0400

    Added the Input File

--- input.cpp
+++ input.cpp
@@ -1,17 +1,52 @@
 #include "input.hpp"
+#include <SDL2/SDL.h>
+
+// Assuming these are defined elsewhere, such as in a header or another source file
+extern int rectX;
+extern int rectY;
+const int MOVE_SPEED = 5;
 
 void doInput(void)
 {
-	SDL_Event event;
+    SDL_Event event;
 
-	while (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-		case SDL_QUIT:
-			exit(0);
-		default:
-			break;
-		}
-	}
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            exit(0);
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym)
+            {
+            case SDLK_UP:
+                if (rectY - MOVE_SPEED >= 0) // Check upper boundary
+                {
+                    rectY -= MOVE_SPEED;
+                }
+                break;
+            case SDLK_DOWN:
+                if (rectY + 400 + MOVE_SPEED <= SCREEN_HEIGHT) // Check lower boundary
+                {
+                    rectY += MOVE_SPEED;
+                }
+                break;
+            case SDLK_LEFT:
+                if (rectX - MOVE_SPEED >= 0) // Check left boundary
+                {
+                    rectX -= MOVE_SPEED;
+                }
+                break;
+            case SDLK_RIGHT:
+                if (rectX + 200 + MOVE_SPEED <= SCREEN_WIDTH) // Check right boundary
+                {
+                    rectX += MOVE_SPEED;
+                }
+                break;
+            }
+            break;
+        default:
+            break;
+        }
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
 #include "main.hpp"
 #include <memory>
 
-int main(int argc, char* argv[]) 
+int rectX = (SCREEN_WIDTH / 2) - 100; // Initialize X position
+int rectY = (SCREEN_HEIGHT / 2) - 200; // Initialize Y position
+
+int main(int argc, char* argv[])
 {
-	initSDL();
+    initSDL();
 
-	SDL_Color screenColor;
-	screenColor.r = 0x00;
-	screenColor.g = 0x00;
-	screenColor.b = 0xFF;
-	screenColor.a = 0x00;
+    SDL_Color screenColor;
+    screenColor.r = 0x00;
+    screenColor.g = 0x00;
+    screenColor.b = 0xFF;
+    screenColor.a = 0x00;
 
-	SDL_Color rectColor;
-	rectColor.r = 0xFF;
-	rectColor.g = 0x00;
-	rectColor.b = 0x00;
-	rectColor.a = 0x00;
+    SDL_Color rectColor;
+    rectColor.r = 0xFF;
+    rectColor.g = 0x00;
+    rectColor.b = 0x00;
+    rectColor.a = 0x00;
 
-	while (1)
-	{
-		prepareScene(screenColor);
+    while (1)
+    {
+        prepareScene(screenColor);
 
-		doInput();
+        doInput();
 
-		drawRect((SCREEN_WIDTH / 2) - 100, (SCREEN_HEIGHT / 2) - 200, 200, 400);
+        drawRect(rectX, rectY, 200, 400); // Use rectX and rectY
 
-		colorRect(rectColor);
+        colorRect(rectColor);
 
-		presentScene();
-	}
+        presentScene();
+    }
 
-	return 0;
+    return 0;
 }
