7eadb3d5669463221c59587cfa7894180ec0a401
Divit <divitkalathil@gmail.com>
2024-08-31 19:35:29 -0400

    Replaced Events with ScanCode

--- input.cpp
+++ input.cpp
@@ -8,45 +8,52 @@ const int MOVE_SPEED = 5;
 
 void doInput(void)
 {
-    SDL_Event event;
+    // Update the state of the keyboard
+    const Uint8* state = SDL_GetKeyboardState(NULL);
 
+    // Check if the SDL_QUIT event has been triggered
+    SDL_Event event;
     while (SDL_PollEvent(&event))
     {
-        switch (event.type)
+        if (event.type == SDL_QUIT)
         {
-        case SDL_QUIT:
             exit(0);
-        case SDL_KEYDOWN:
-            switch (event.key.keysym.sym)
-            {
-            case SDLK_UP:
-                if (rectY - MOVE_SPEED >= 0) // Check upper boundary
-                {
-                    rectY -= MOVE_SPEED;
-                }
-                break;
-            case SDLK_DOWN:
-                if (rectY + 400 + MOVE_SPEED <= SCREEN_HEIGHT) // Check lower boundary
-                {
-                    rectY += MOVE_SPEED;
-                }
-                break;
-            case SDLK_LEFT:
-                if (rectX - MOVE_SPEED >= 0) // Check left boundary
-                {
-                    rectX -= MOVE_SPEED;
-                }
-                break;
-            case SDLK_RIGHT:
-                if (rectX + 200 + MOVE_SPEED <= SCREEN_WIDTH) // Check right boundary
-                {
-                    rectX += MOVE_SPEED;
-                }
-                break;
-            }
-            break;
-        default:
-            break;
+        }
+    }
+
+    // Check if the UP key is pressed
+    if (state[SDL_SCANCODE_UP])
+    {
+        if (rectY - MOVE_SPEED >= 0) // Check upper boundary
+        {
+            rectY -= MOVE_SPEED;
+        }
+    }
+
+    // Check if the DOWN key is pressed
+    if (state[SDL_SCANCODE_DOWN])
+    {
+        if (rectY + 400 + MOVE_SPEED <= SCREEN_HEIGHT) // Check lower boundary
+        {
+            rectY += MOVE_SPEED;
+        }
+    }
+
+    // Check if the LEFT key is pressed
+    if (state[SDL_SCANCODE_LEFT])
+    {
+        if (rectX - MOVE_SPEED >= 0) // Check left boundary
+        {
+            rectX -= MOVE_SPEED;
+        }
+    }
+
+    // Check if the RIGHT key is pressed
+    if (state[SDL_SCANCODE_RIGHT])
+    {
+        if (rectX + 200 + MOVE_SPEED <= SCREEN_WIDTH) // Check right boundary
+        {
+            rectX += MOVE_SPEED;
         }
     }
 }
