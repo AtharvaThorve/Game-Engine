d69dee1d8157ec55816b1ca9f9ec4d97db60aa9e
Atharva Thorve <aaathorve@gmail.com>
2024-09-02 23:48:38 -0400

    Initialization issue in entity fixed

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
     float scale = 1.0f;
     float cached_scale = scale;
 
-    PhysicsSystem physicsSystem(0.0f, 9.8f);
+    PhysicsSystem physicsSystem(50.0f, 100.0f);
 
     Vector2 initialPosition{ 100, 100 };
     Vector2 initialVelocity{ 0, 0 };
@@ -29,21 +29,21 @@ int main(int argc, char* argv[])
 
     EntityManager entityManager;
 
-    auto entity = std::make_shared<Entity>(initialPosition, initialVelocity, inputInitialVelocity, mass, isAffectedByGravity, isMovable, isHittable, shapeType, color, rect, center, radius);
-    auto entity1 = std::make_shared<Entity>(initialPosition1, initialVelocity, inputInitialVelocity, mass, isAffectedByGravity, isMovable, isHittable, shapeType, color, rect1, center, radius);
+    auto entity = std::make_shared<Entity>(initialPosition, initialVelocity, mass, isAffectedByGravity, isMovable, isHittable, shapeType, color, rect, center, radius);
+    auto entity1 = std::make_shared<Entity>(initialPosition1, initialVelocity, mass, isAffectedByGravity, isMovable, isHittable, shapeType, color, rect1, center, radius);
 
     Vector2 initialPosition2{ 300, 300 };
     SDL_Rect rect2 = { static_cast<int>(initialPosition2.x), static_cast<int>(initialPosition2.y), 50, 50 };
-    auto patternEntity = std::make_shared<Entity>(initialPosition2, initialVelocity, inputInitialVelocity, mass, !isAffectedByGravity, isMovable, isHittable, shapeType, color, rect2, center, radius);
+    auto patternEntity = std::make_shared<Entity>(initialPosition2, initialVelocity, mass, !isAffectedByGravity, isMovable, isHittable, shapeType, color, rect2, center, radius);
 
     Vector2 initialPosition3{ 600, 100 };
     SDL_Rect rect3 = { static_cast<int>(initialPosition3.x), static_cast<int>(initialPosition3.y), 50, 50 };
-    auto patternEntity1 = std::make_shared<Entity>(initialPosition3, initialVelocity, inputInitialVelocity, mass, isAffectedByGravity, isMovable, isHittable, shapeType, color, rect3, center, radius);
+    auto patternEntity1 = std::make_shared<Entity>(initialPosition3, initialVelocity, mass, isAffectedByGravity, isMovable, isHittable, shapeType, color, rect3, center, radius);
 
     MovementPattern pattern;
-    pattern.addStep(MovementStep({ 50, 50 }, 2.0f)); // Move right for 2 secs at an acceleration of 50
+    pattern.addStep(MovementStep({ 50, 50 }, 2.0f)); // Move diagonally right and down for 2 secs at an acceleration of 50
     pattern.addStep(MovementStep({ 0 , 0 }, 1.0f, true)); // Stop for 1 sec
-    pattern.addStep(MovementStep({ -50, -50 }, 2.0f)); // Move left for 2 secs at an acceleratopn of 50
+    pattern.addStep(MovementStep({ -50, -50 }, 2.0f)); // Move diagonally left and up for 2 secs at an acceleratopn of 50
     pattern.addStep(MovementStep({ 0 , 0 }, 1.0f, true)); // Stop for 1 sec
     patternEntity->hasMovementPattern = true;
     patternEntity->movementPattern = pattern;
@@ -61,7 +61,7 @@ int main(int argc, char* argv[])
     while (1)
     {
         // Handle input, which might modify the entity's velocity
-        doInput(entity, 50.0f, 100.0f);
+        doInput(entity, 50.0f, 150.0f);
 
         Uint32 currentTime = SDL_GetTicks();
         float deltaTime = (currentTime - lastTime) / 1000.0f; // Time in seconds since last frame
